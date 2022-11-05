feaf2bf606451030d8521ba0efe81f6a4fda5a3f
unknown <abhiramkrishna085@gmail.com>
2022-11-05 14:52:25 +0530

    Updates

---
+++ precache-manifest.bef72d436ea395cb8d1ae0aa9d4e7bb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c216da8eaf8fda113eb",
+    "url": "/my-app/static/css/main.1c075670.chunk.css"
+  },
+  {
+    "revision": "8c216da8eaf8fda113eb",
+    "url": "/my-app/static/js/main.8c216da8.chunk.js"
+  },
+  {
+    "revision": "285985289364e02847a3",
+    "url": "/my-app/static/js/1.28598528.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "34c09d8f364111e1418009eb19417949",
+    "url": "/my-app/index.html"
+  }
+];
