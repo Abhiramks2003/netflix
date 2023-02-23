1363ece4f4bb6d501077b5238c447c2eb600191b
ABHIRAM KRISHNA S <abhiramkrishna085@gmail.com>
2023-02-23 19:05:54 +0530

    Website is now responsive.

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './RowPost.css'
-import {imageUrl} from "../../constants/constants";
+import { imageUrl } from "../../constants/constants";
 import axios from '../../axios'
 function RowPost(props) {
     const [movies, setMovies] = useState([]);
@@ -10,14 +10,15 @@ function RowPost(props) {
         }).catch(err => {
             alert('error')
         })
-    },[])
+    }, [])
 
+ 
     return (
         <div className='row'>
             <h2>{props.title}</h2>
             <div className="posters">
-                {movies.map((obj) => 
-                    <img className={props.isSmall ?'smallposter' :'poster' } src={`${imageUrl + obj.backdrop_path}`} alt="poster" />
+                {movies.map((obj) =>
+                    <img className={props.isSmall ? 'smallposter' : 'poster'} src={`${imageUrl + obj.backdrop_path}`} alt="poster" />
                 )}
             </div>
         </div>
